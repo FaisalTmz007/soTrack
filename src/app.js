772ad88d0852267729b17a3807b5518c4911df0c
Muhamad Faisal Fachrul Ulum <faisaltemas007@gmail.com>
2024-05-27 14:24:49 +0700

    refactor: Update sentiment endpoint in sotrack.py

--- src/app.js
+++ src/app.js
@@ -50,7 +50,7 @@ passport.deserializeUser(function (obj, cb) {
 
 app.use(express.json());
 app.use(cors(corsOptions));
-app.use(morgan("combined"));
+app.use(morgan("short"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
--- src/controllers/dashboard/sentimentAnalysis.js
+++ src/controllers/dashboard/sentimentAnalysis.js
@@ -4,15 +4,134 @@ const jwt = require("jsonwebtoken");
 const axios = require("axios");
 const translate = require("translate-google");
 
+const getSentimentPercentages = (items) => {
+  const sentimentCounts = items.reduce((acc, item) => {
+    acc[item.sentiment] = (acc[item.sentiment] || 0) + 1;
+    return acc;
+  }, {});
+
+  const totalItems = items.length;
+  return Object.entries(sentimentCounts).reduce((acc, [sentiment, count]) => {
+    acc[sentiment] = ((count / totalItems) * 100).toFixed(2) + "%";
+    return acc;
+  }, {});
+};
+
+const processFilters = async (filters, getPosts, facebook_access_token) => {
+  let allPosts = [];
+
+  await Promise.all(
+    filters.map(async (filter) => {
+      try {
+        const posts = await getPosts(filter, facebook_access_token);
+        allPosts.push(...posts);
+      } catch (error) {
+        console.error(`Error processing filter ${filter.id}: `, error);
+      }
+    })
+  );
+
+  return allPosts;
+};
+
+const translateAndAnalyzeSentiments = async (captions, startIndex = 0) => {
+  const batchSize = 10; // Adjust the batch size as needed
+  const sentiments = [];
+
+  for (let i = 0; i < captions.length; i += batchSize) {
+    const batch = captions.slice(i, i + batchSize);
+    const translatedCaptions = await Promise.all(
+      batch.map((caption) => translate(caption, { from: "id", to: "en" }))
+    );
+
+    const response = await axios.post(`${process.env.FLASK_URL}/sentiment`, {
+      headlines: translatedCaptions,
+    });
+
+    sentiments.push(...response.data.sentiments.map((s) => s.category));
+  }
+
+  return sentiments;
+};
+
+const getInstagramMentionPosts = async (
+  filter,
+  accessToken,
+  fromUnix,
+  toUnix
+) => {
+  const postsResponse = await axios.get(
+    `https://graph.facebook.com/v19.0/${filter.id}/tags`,
+    {
+      params: {
+        fields: `id,caption,timestamp`,
+        access_token: accessToken,
+        since: fromUnix,
+        until: toUnix,
+      },
+    }
+  );
+
+  const posts = postsResponse.data.data;
+
+  if (posts.length === 0) return [];
+
+  const captions = posts.map((post) => post.caption || "no caption");
+  const sentiments = await translateAndAnalyzeSentiments(captions);
+
+  posts.forEach((post, index) => {
+    post.sentiment = sentiments[index];
+  });
+
+  return posts;
+};
+
+const getInstagramHashtagPosts = async (filter, accessToken, userId) => {
+  const hashtagIdResponse = await axios.get(
+    `https://graph.facebook.com/v19.0/ig_hashtag_search`,
+    {
+      params: {
+        user_id: userId,
+        q: filter.parameter,
+        access_token: accessToken,
+      },
+    }
+  );
+
+  const hashtagId = hashtagIdResponse.data.data[0].id;
+
+  const postsResponse = await axios.get(
+    `https://graph.facebook.com/v19.0/${hashtagId}/recent_media`,
+    {
+      params: {
+        user_id: userId,
+        fields: "id,caption,timestamp",
+        access_token: accessToken,
+      },
+    }
+  );
+
+  const posts = postsResponse.data.data;
+
+  if (posts.length === 0) return [];
+
+  const captions = posts.map((post) => post.caption || "no caption");
+  const sentiments = await translateAndAnalyzeSentiments(captions);
+
+  posts.forEach((post, index) => {
+    post.sentiment = sentiments[index];
+  });
+
+  return posts;
+};
+
 const sentimentAnalysis = async (req, res) => {
   try {
     const refreshToken = req.cookies.refresh_token;
     const { platform, from, to } = req.query;
 
     if (!refreshToken) {
-      return res.status(400).json({
-        message: "Please login first",
-      });
+      return res.status(400).json({ message: "Please login first" });
     }
 
     const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
@@ -23,367 +142,171 @@ const sentimentAnalysis = async (req, res) => {
       });
     }
 
+    const fromUnix = Math.floor(new Date(from).getTime() / 1000);
+    const toUnix = Math.floor(new Date(to).getTime() / 1000);
+
+    let filters, getPosts;
+
     if (platform === "news") {
-      const filter = await prisma.Filter.findMany({
+      filters = await prisma.Filter.findMany({
         where: {
           is_active: true,
           user_id: decoded.id,
-          Platform: {
-            name: "News",
-          },
-          Category: {
-            name: "Keyword",
-          },
+          Platform: { name: "News" },
+          Category: { name: "Keyword" },
         },
       });
 
-      let allNews = [];
-
-      if (filter.length > 0) {
-        await Promise.all(
-          filter.map(async (f) => {
-            const news = await prisma.News.findMany({
-              where: {
-                title: {
-                  contains: f.keyword,
-                },
-                published_at: {
-                  gte: new Date(from),
-                  lte: new Date(to),
-                },
-              },
-            });
-
-            if (news.length === 0) return [];
-
-            const translatedNews = await Promise.all(
-              news.map(async (n) => {
-                const caption = n.title || n.caption;
-                const translatedCaption = await translate(caption, {
-                  from: "id",
-                  to: "en",
-                });
-
-                const sentiment = await axios.post(
-                  `${process.env.FLASK_URL}/sentiment`,
-                  {
-                    headline: translatedCaption,
-                  }
-                );
-
-                // console.log(sentiment.data.category);
-
-                n.sentiment = sentiment.data.category;
-
-                return n;
-              })
-            );
-
-            allNews.push(...translatedNews);
-          })
-        );
-      }
-
-      if (allNews.length === 0) {
-        return res.status(404).json({
-          message: "No news found",
+      getPosts = async (filter) => {
+        const items = await prisma.News.findMany({
+          where: {
+            title: { contains: filter.keyword },
+            published_at: { gte: new Date(from), lte: new Date(to) },
+          },
         });
-      }
 
-      // Calculate sentiment percentages
-      const sentimentCounts = allNews.reduce((acc, article) => {
-        acc[article.sentiment] = (acc[article.sentiment] || 0) + 1;
-        return acc;
-      }, {});
+        if (items.length === 0) return [];
 
-      const totalArticles = allNews.length;
-      const sentimentPercentages = Object.entries(sentimentCounts).reduce(
-        (acc, [sentiment, count]) => {
-          acc[sentiment] = ((count / totalArticles) * 100).toFixed(2) + "%";
-          return acc;
-        },
-        {}
-      );
+        const captions = items.map((item) => item.title || "no caption");
+        const sentiments = await translateAndAnalyzeSentiments(captions);
 
-      res.json({
-        message: "All news",
-        data: sentimentPercentages,
-      });
+        items.forEach((item, index) => {
+          item.sentiment = sentiments[index];
+        });
+
+        return items;
+      };
     } else if (platform === "facebook") {
       const facebook_access_token = req.cookies.facebook_access_token;
 
       if (!facebook_access_token) {
-        return res.status(400).json({
-          message: "Please login first",
-        });
+        return res.status(400).json({ message: "Please login first" });
       }
 
-      // convert from adn to into UNIX
-      const fromUnix = Math.floor(new Date(from).getTime() / 1000);
-      const toUnix = Math.floor(new Date(to).getTime() / 1000);
-
-      const filter = await prisma.Filter.findMany({
+      filters = await prisma.Filter.findMany({
         where: {
           is_active: true,
           user_id: decoded.id,
-          Platform: {
-            name: "Facebook",
-          },
-          Category: {
-            name: "Mention",
-          },
+          Platform: { name: "Facebook" },
+          Category: { name: "Mention" },
         },
       });
 
-      let allPosts = [];
-
-      if (filter.length > 0) {
-        await Promise.all(
-          filter.map(async (f) => {
-            const page_token_response = await axios.get(
-              `https://graph.facebook.com/v19.0/${f.id}`,
-              {
-                params: {
-                  fields: "access_token",
-                  access_token: facebook_access_token,
-                },
-              }
-            );
-
-            const page_token = page_token_response.data.access_token;
-
-            const posts_response = await axios.get(
-              `https://graph.facebook.com/v19.0/${f.id}/tagged`,
-              {
-                params: {
-                  fields: `id, message, created_time, permalink_url`,
-                  since: fromUnix,
-                  until: toUnix,
-                  access_token: page_token,
-                },
-              }
-            );
-
-            const posts = posts_response.data.data;
-
-            if (posts.length === 0) return [];
-
-            const translatedPosts = await Promise.all(
-              posts.map(async (p) => {
-                const caption = p.message || "no caption";
-                const translatedMessage = await translate(caption, {
-                  from: "id",
-                  to: "en",
-                });
-
-                // console.log(translatedMessage);
-
-                const sentiment = await axios.post(
-                  `${process.env.FLASK_URL}/sentiment`,
-                  {
-                    headline: translatedMessage,
-                  }
-                );
-
-                p.sentiment = sentiment.data.category;
-
-                // console.log(sentiment.data.processed_text);
-                // console.log(sentiment.data.category);
-
-                return p;
-              })
-            );
-
-            allPosts.push(...translatedPosts);
-          })
+      getPosts = async (filter, accessToken) => {
+        const pageTokenResponse = await axios.get(
+          `https://graph.facebook.com/v19.0/${filter.id}`,
+          {
+            params: {
+              fields: "access_token",
+              access_token: accessToken,
+            },
+          }
         );
-      }
 
-      if (allPosts.length === 0) {
-        return res.status(404).json({
-          message: "No posts found",
+        const pageToken = pageTokenResponse.data.access_token;
+
+        const postsResponse = await axios.get(
+          `https://graph.facebook.com/v19.0/${filter.id}/tagged`,
+          {
+            params: {
+              fields: "id,message,created_time,permalink_url",
+              since: fromUnix,
+              until: toUnix,
+              access_token: pageToken,
+            },
+          }
+        );
+
+        const posts = postsResponse.data.data;
+
+        if (posts.length === 0) return [];
+
+        const captions = posts.map((post) => post.message || "no caption");
+        const sentiments = await translateAndAnalyzeSentiments(captions);
+
+        posts.forEach((post, index) => {
+          post.sentiment = sentiments[index];
         });
-      }
 
-      // Calculate sentiment percentages
-      const sentimentCounts = allPosts.reduce((acc, post) => {
-        acc[post.sentiment] = (acc[post.sentiment] || 0) + 1;
-        return acc;
-      }, {});
-
-      const totalPosts = allPosts.length;
-      const sentimentPercentages = Object.entries(sentimentCounts).reduce(
-        (acc, [sentiment, count]) => {
-          acc[sentiment] = ((count / totalPosts) * 100).toFixed(2) + "%";
-          return acc;
-        },
-        {}
-      );
-
-      res.json({
-        message: "All posts",
-        data: sentimentPercentages,
-      });
+        return posts;
+      };
     } else if (platform === "instagram") {
       const facebook_access_token = req.cookies.facebook_access_token;
 
       if (!facebook_access_token) {
-        return res.status(400).json({
-          message: "Please login first",
-        });
+        return res.status(400).json({ message: "Please login first" });
       }
 
-      // convert from adn to into UNIX
-      const fromUnix = Math.floor(new Date(from).getTime() / 1000);
-      const toUnix = Math.floor(new Date(to).getTime() / 1000);
-
-      let allPosts = [];
-
-      const mentionFilter = await prisma.Filter.findMany({
+      filters = await prisma.Filter.findMany({
         where: {
           is_active: true,
           user_id: decoded.id,
-          Platform: {
-            name: "Instagram",
-          },
-          Category: {
-            name: "Mention",
-          },
+          Platform: { name: "Instagram" },
+          Category: { name: "Mention" },
         },
       });
 
-      if (mentionFilter.length > 0) {
-        await Promise.all(
-          mentionFilter.map(async (item) => {
-            const posts = await axios.get(
-              `https://graph.facebook.com/v19.0/${item.id}/tags`,
-              {
-                params: {
-                  fields: `id,caption,timestamp`,
-                  access_token: facebook_access_token,
-                },
-              }
-            );
-
-            if (posts.data.data.length === 0) return [];
-
-            const translatedPosts = await Promise.all(
-              posts.data.data.map(async (post) => {
-                const caption = post.caption || "no caption";
-                const translatedCaption = await translate(caption, {
-                  from: "id",
-                  to: "en",
-                });
-
-                const sentiment = await axios.post(
-                  `${process.env.FLASK_URL}/sentiment`,
-                  {
-                    headline: translatedCaption,
-                  }
-                );
-
-                post.sentiment = sentiment.data.category;
-
-                return post;
-              })
-            );
-
-            allPosts.push(...translatedPosts);
-          })
-        );
-      }
-
-      const hashtagFilter = await prisma.Filter.findMany({
-        where: {
-          is_active: true,
-          user_id: decoded.id,
-          Platform: {
-            name: "Instagram",
-          },
-          Category: {
-            name: "Hashtag",
-          },
-        },
-      });
-
-      if (hashtagFilter.length > 0) {
-        await Promise.all(
-          hashtagFilter.map(async (f) => {
-            const hashtagId = await axios.get(
-              `https://graph.facebook.com/v19.0/ig_hashtag_search`,
-              {
-                params: {
-                  user_id: mentionFilter[0].id,
-                  q: f.parameter,
-                  access_token: facebook_access_token,
-                },
-              }
-            );
-
-            const posts = await axios.get(
-              `https://graph.facebook.com/v19.0/${hashtagId.data.data[0].id}/recent_media`,
-              {
-                params: {
-                  user_id: mentionFilter[0].id,
-                  fields: "timestamp",
-                  access_token: facebook_access_token,
-                },
-              }
-            );
-
-            if (posts.data.data.length === 0) return [];
-
-            const translatedPosts = await Promise.all(
-              posts.data.data.map(async (p) => {
-                const caption = p.caption || "no caption";
-                const translatedCaption = await translate(caption, {
-                  from: "id",
-                  to: "en",
-                });
-
-                const sentiment = await axios.post(
-                  `${process.env.FLASK_URL}/sentiment`,
-                  {
-                    headline: translatedCaption,
-                  }
-                );
-
-                p.sentiment = sentiment.data.category;
-
-                return p;
-              })
-            );
-
-            allPosts.push(...translatedPosts);
-          })
-        );
-      }
-
-      // Calculate sentiment percentages
-      const sentimentCounts = allPosts.reduce((acc, post) => {
-        acc[post.sentiment] = (acc[post.sentiment] || 0) + 1;
-        return acc;
-      }, {});
-
-      const totalPosts = allPosts.length;
-      const sentimentPercentages = Object.entries(sentimentCounts).reduce(
-        (acc, [sentiment, count]) => {
-          acc[sentiment] = ((count / totalPosts) * 100).toFixed(2) + "%";
-          return acc;
-        },
-        {}
+      const mentionPosts = await processFilters(
+        filters,
+        async (filter) =>
+          getInstagramMentionPosts(
+            filter,
+            facebook_access_token,
+            fromUnix,
+            toUnix
+          ),
+        facebook_access_token
       );
 
-      res.json({
-        message: "All posts",
-        data: sentimentPercentages,
+      const hashtagFilters = await prisma.Filter.findMany({
+        where: {
+          is_active: true,
+          user_id: decoded.id,
+          Platform: { name: "Instagram" },
+          Category: { name: "Hashtag" },
+        },
       });
+
+      const hashtagPosts = await processFilters(
+        hashtagFilters,
+        async (filter) =>
+          getInstagramHashtagPosts(
+            filter,
+            facebook_access_token,
+            filters[0].id
+          ),
+        facebook_access_token
+      );
+
+      const allPosts = mentionPosts.concat(hashtagPosts);
+
+      if (allPosts.length === 0) {
+        return res.status(404).json({ message: "No posts found" });
+      }
+
+      const sentimentPercentages = getSentimentPercentages(allPosts);
+
+      return res.json({ message: "All posts", data: { sentimentPercentages } });
+    } else {
+      return res.status(400).json({ message: "Invalid platform" });
     }
+
+    const allPosts = await processFilters(
+      filters,
+      getPosts,
+      req.cookies.facebook_access_token
+    );
+
+    if (allPosts.length === 0) {
+      return res.status(404).json({ message: "No posts found" });
+    }
+
+    const sentimentPercentages = getSentimentPercentages(allPosts);
+
+    res.json({ message: "All posts", data: sentimentPercentages });
   } catch (error) {
     console.error(error);
-    res.status(500).json({
-      message: "Internal server error",
-    });
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
